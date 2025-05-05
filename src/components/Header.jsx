d48959fbead13286b2c6504b806962472ae35f53
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-06 01:52:55 +0200

    [UPD] Impostata struttura header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src="dc-logo.png" alt="Logo dc" />
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#" className="active">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
