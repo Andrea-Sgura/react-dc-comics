fab05f436173bf142eb3c00b31bf42fed63965b7
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-06 02:17:14 +0200

    [UPD] Impostata struttura Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+const Main = () => {
+    return (
+        <main>
+            <div>
+                <h2>Content Goes Here</h2>
+            </div>
+            <figure>
+                <img src="buy-comics-digital-comics.png" alt="Digital Comics" />
+                <h4>Digital Comics</h4>
+            </figure>
+        </main>
+    )
+}
+
+export default Main
