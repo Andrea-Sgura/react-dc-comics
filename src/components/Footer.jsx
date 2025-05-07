f05a466bebee6c293d8a558c615315ea1f17a8b7
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-07 04:21:49 +0200

    [UPD] Impostata struttura Footer, importato in app.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="top-footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-100">
+                            <div className="footer-menu"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom-footer"></div>
+        </footer>
+    )
+}
+
+export default Footer
