41af7c6a64d0c9bac45385d3ce0c66934f8d7657
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-07 23:44:19 +0200

    [UPD] Creato file listcomics.jsx e importato array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
+import BlueSection from "./components/BlueSection"
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <BlueSection />
       <Footer />
     </>)
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,17 @@
+import ListComics from "./ListComics"
+
 const Main = () => {
     return (
         <>
             <main>
                 <div className="container">
                     <div className="row">
-                        <div className="col-100">Content Goes Here</div>
+                        <div className="col-100">
+                            <ListComics />
+                        </div>
                     </div>
                 </div>
             </main>
-            <div className="container">
-                <div className="blue-section d-flex justify-between">
-                    <div className="cta">
-                        <img className="png-image" src="src/assets/img/buy-comics-digital-comics.png" alt="DC Comics" />
-                    </div>
-                    <div className="cta">
-                        <img className="png-image" src="src/assets/img/buy-comics-merchandise.png" alt="DC Comics" />
-                    </div>
-                    <div className="cta">
-                        <img className="png-image" src="src/assets/img/buy-comics-subscriptions.png" alt="DC Comics" />
-                    </div>
-                    <div className="cta">
-                        <img className="png-image" src="src/assets/img/buy-comics-shop-locator.png" alt="DC Comics" />
-                    </div>
-                    <div className="cta">
-                        <img className="svg-image" src="src/assets/img/buy-dc-power-visa.svg" alt="DC Comics" />
-                    </div>
-                </div>
-            </div>
         </>
     )
 }
